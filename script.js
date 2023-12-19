6b693935b5b99041a2ddf65bd47f0fb05aff1dca
Juha Ala-Rantala <alarantala.juha@gmail.com>
2023-12-19 14:53:24 +0200

    working on layout and styling

---
+++ script.js
@@ -0,0 +1,19 @@
+const projectFilters = document.getElementsByClassName("project-filter");
+for (let filter of projectFilters) {
+  const buttonElement = document.getElementById(filter.id);
+  buttonElement.addEventListener("click", function () {
+    // if already active, do nothing
+    if (buttonElement.classList.contains("project-filter-enabled")) {
+      return;
+    }
+
+    // remove all active classes from buttons
+    for (let filter of projectFilters) {
+      const otherButtonElement = document.getElementById(filter.id);
+      otherButtonElement.classList.remove("project-filter-enabled");
+    }
+
+    // add active class to clicked button
+    buttonElement.classList.add("project-filter-enabled");
+  });
+}
