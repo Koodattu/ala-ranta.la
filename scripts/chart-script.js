ee1038d64912faff307a2db7ba5385a4237015fe
Juha Ala-Rantala <alarantala.juha@gmail.com>
2024-02-05 19:51:48 +0200

    Update chart layout and add collapsible functionality

--- scripts/chart-script.js
+++ scripts/chart-script.js
@@ -161,3 +161,18 @@ function createPieChart(selector, data, category) {
     .style("font-family", "Arial")
     .style("font-size", "14px");
 }
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
--- scripts/project-filters.js
+++ scripts/project-filters.js
@@ -4,16 +4,14 @@ function filterProjects() {
 
   projects.forEach((project) => {
     const tags = Array.from(project.querySelectorAll(".tech-tag")).map((tag) => tag.textContent.toLowerCase());
-    let isVisible = Array.from(activeFilters).some((filter) => tags.includes(filter.textContent.toLowerCase()));
-
     const filterMatched = Array.from(activeFilters).every((filter) => tags.includes(filter.textContent.toLowerCase()));
     if (filterMatched || activeFilters.length === 0) {
       project.style.display = "block";
     } else {
       project.style.display = "none";
     }
-    updateDonutChart();
   });
+  calculateVisibleProjects();
 }
 
 // Call filterProjects whenever a filter button is clicked
@@ -63,11 +61,11 @@ function populateFilterTags() {
       };
 
       // Populate each category
-      appendTags(".project-filters-platform > div", platforms);
-      appendTags(".project-filters-language > div", languages);
-      appendTags(".project-filters-frontend > div", frontends);
-      appendTags(".project-filters-backend > div", backends);
-      appendTags(".project-filters-tools > div", tools);
+      appendTags(".project-filters-platform", platforms);
+      appendTags(".project-filters-language", languages);
+      appendTags(".project-filters-frontend", frontends);
+      appendTags(".project-filters-backend", backends);
+      appendTags(".project-filters-tools", tools);
 
       // Add event listeners to each tag button
       projectFilterHandler();
@@ -86,102 +84,12 @@ document.getElementById("select-none-filter").addEventListener("click", disableA
 populateFilterTags();
 addFilteredProjects("all");
 
-function createDonutChart(selector, data) {
-  const width = 150,
-    height = 150,
-    margin = 12;
-  const radius = Math.min(width, height) / 2 - margin;
-
-  const color = d3.scaleOrdinal().range(["#5fc9f9", "#ccc"]); // Colors for visible and hidden projects
-
-  const svg = d3
-    .select(selector)
-    .append("svg")
-    .attr("width", width)
-    .attr("height", height)
-    .append("g")
-    .attr("transform", `translate(${width / 2}, ${height / 2})`);
-
-  const pie = d3.pie().value((d) => d.value);
-  const data_ready = pie(data);
-
-  const arc = d3
-    .arc()
-    .innerRadius(radius * 0.5)
-    .outerRadius(radius * 0.8);
-  const outerArc = d3
-    .arc()
-    .innerRadius(radius * 0.9)
-    .outerRadius(radius * 0.9);
-
-  svg
-    .selectAll("path")
-    .data(data_ready)
-    .join("path")
-    .attr("d", arc)
-    .attr("fill", (d) => color(d.data.key));
-
-  svg
-    .selectAll("text")
-    .data(data_ready)
-    .join("text")
-    .text((d) => `${d.data.key}: ${d.data.value}`)
-    .attr("transform", (d) => {
-      const pos = outerArc.centroid(d);
-      const midangle = d.startAngle + (d.endAngle - d.startAngle) / 2;
-      pos[0] = radius * (midangle < Math.PI ? 1 : -1);
-      return `translate(${pos})`;
-    })
-    .style("text-anchor", (d) => {
-      const midangle = d.startAngle + (d.endAngle - d.startAngle) / 2;
-      return midangle < Math.PI ? "start" : "end";
-    })
-    .style("fill", "whitesmoke")
-    .style("font-family", "Arial")
-    .style("font-size", "14px");
-
-  // Draw polylines connecting slices to labels
-  svg
-    .selectAll("polyline")
-    .data(data_ready)
-    .join("polyline")
-    .attr("stroke", "whitesmoke")
-    .style("fill", "none")
-    .attr("stroke-width", 1)
-    .attr("points", (d) => {
-      const posA = arc.centroid(d);
-      const posB = outerArc.centroid(d);
-      const posC = outerArc.centroid(d);
-      const midangle = d.startAngle + (d.endAngle - d.startAngle) / 2;
-      posC[0] = radius * 0.95 * (midangle < Math.PI ? 1 : -1);
-      return [posA, posB, posC];
-    });
-}
-
-// Function to update the donut chart based on filters
-function updateDonutChart() {
+function calculateVisibleProjects() {
   const visibleProjects = document.querySelectorAll(".project-container[style='display: block;']");
   const hiddenProjects = document.querySelectorAll(".project-container[style='display: none;']");
 
-  const data = [
-    { key: "Visible", value: visibleProjects.length },
-    { key: "Hidden", value: hiddenProjects.length },
-  ];
-
-  d3.select("#visibility-chart svg").remove(); // Clear the previous chart
-  createDonutChart("#visibility-chart", data);
+  const visibleProjectsText = document.getElementById("visible-projects");
+  visibleProjectsText.textContent = `Currently showing ${visibleProjects.length} of ${
+    visibleProjects.length + hiddenProjects.length
+  } projects. (${hiddenProjects.length} hidden)`;
 }
-
-function createInitialDonutChart() {
-  fetch("./data/projects.json")
-    .then((response) => response.json())
-    .then((jsonResponse) => {
-      const data = [
-        { key: "Visible", value: jsonResponse.projects.length },
-        { key: "Hidden", value: 0 },
-      ];
-      createDonutChart("#visibility-chart", data);
-    });
-}
-
-createInitialDonutChart();
